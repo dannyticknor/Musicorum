35cf3c7863aff66b02de777dd7ac10d5b6b61e63
Rishabh Goel <rishabh297@berkeley.edu>
2021-02-20 10:31:42 -0800

    done first file

---
+++ myfirst/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String wedmadeit = "Hii guysss";
+	System.out.println(wedmadeit);
+    }
+}
---
+++ myfirst/whatever.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String wedmadeit = "Hii guysss";
+        System.out.println(wedmadeit);
+    }
+}
